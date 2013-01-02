66ec0d36c2e362c559b95af988c523bcb45b7178
markeeftb <marek@ftb.pl>
2013-01-02 14:14:39 -0800

    Create fileopener.js
    Simple javascript function FileOpener with only one method 'open' to open local file.

---
+++ fileopener.js
@@ -0,0 +1,28 @@
+/*
+ * PhoneGap is available under *either* the terms of the modified BSD license *or* the
+ * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
+ *
+ * Copyright (c) 2005-2010, Nitobi Software Inc.
+ * Copyright (c) 2011, IBM Corporation
+ */
+
+/**
+ * Constructor
+ */
+function FileOpener() {
+};
+
+FileOpener.prototype.open = function(url) {
+    cordova.exec(null, null, "FileOpener", "openFile", [url]);
+};
+
+/**
+ * Load Plugin
+ */
+
+if(!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.fileOpener) {
+    window.plugins.fileOpener = new FileOpener();
+}
